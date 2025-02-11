df208a4f8b95d816be730445e00321ab660fb8ef
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-02-11 05:05:08 +0500

    post new launch addd at client side

--- Client/src/hooks/requests.js
+++ Client/src/hooks/requests.js
@@ -9,21 +9,38 @@ async function httpGetPlanets() {
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
-  const fetchedLaunches = await response.json()
-
+  const fetchedLaunches = await response.json();
   return fetchedLaunches.sort((a, b) => {
-    return a.flightNumber - b.flightNumber // Sort launches by flight number (ascending)
-  })
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    console.log("Submitting Launch:", launch);
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    await fetch(`${API_URL}/upcoming/:id`, { method: "delete" });
+  } catch (error){
+    error
+    
+  }
 }
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
