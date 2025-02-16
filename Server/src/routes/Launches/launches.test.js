25e545a6efbd323545ec1d0a2549f9d11c857a4c
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-02-17 04:43:38 +0500

    update tests for launches

--- Server/src/routes/Launches/launches.test.js
+++ Server/src/routes/Launches/launches.test.js
@@ -1,71 +1,84 @@
 const request = require("supertest");
 const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
-describe("test GET /launches", () => {
-  test("staus code should be 200", async () => {
-    const response = await request(app).get("/launches").expect(200);
-  });
-});
-
-describe("test POTS /launches", () => {
-  const launchDataWithDate = {
-    mission: "exoplanet 1",
-    target: "Kepler 21",
-    launchDate: "June 20, 2030",
-    rocket: "IS1",
-  };
-  const DataWithOutDate = {
-    mission: "exoplanet 1",
-    target: "Kepler 21",
-    rocket: "IS1",
-  };
-  const dataWithInvalidDate = {
-    mission: "exoplanet 1",
-    target: "Kepler 21",
-    launchDate: "root",
-    rocket: "IS1",
-  }
-
-  test("should be 201", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDataWithDate)
-      .expect(201);
-
-    const requestDate = new Date(launchDataWithDate.launchDate).getTime();
-    const responseDate = new Date(response.body.launchDate).getTime();
-
-    expect(requestDate).toBeCloseTo(responseDate, 3);
-    expect(response.body).toMatchObject(DataWithOutDate);
+describe("Testing Nasa Api", () => {
+  beforeAll(async () => {
+    await mongoConnect();
   });
 
-  test('test POST /launches', async() => {
-    const response = await request(app).post('/launches')
-      .send(dataWithInvalidDate)
-      .expect(400)
+  afterAll(async () => {
+    await mongoDisconnect();
+  });
+
+  describe("test GET /launches", () => {
+    test("staus code should be 200", async () => {
+      const response = await request(app).get("/v1/launches").expect(200);
+    });
+  });
+
+  describe("test POTS /launches", () => {
+    const launchDataWithDate = {
+      mission: "exoplanet 1",
+      target: "Kepler 21",
+      launchDate: "June 20, 2030",
+      rocket: "IS1",
+    };
+    const DataWithOutDate = {
+      mission: "exoplanet 1",
+      target: "Kepler 21",
+      rocket: "IS1",
+    };
+    const dataWithInvalidDate = {
+      mission: "exoplanet 1",
+      target: "Kepler 21",
+      launchDate: "root",
+      rocket: "IS1",
+    };
+
+    test("should be 201", async () => {
+      const response = await request(app)
+        .post("/v1/launches")
+        .send(launchDataWithDate)
+        .expect(201);
+
+      const requestDate = new Date(launchDataWithDate.launchDate).getTime();
+      const responseDate = new Date(response.body.launchDate).getTime();
+
+      expect(requestDate).toBeCloseTo(responseDate, 3);
+      expect(response.body).toMatchObject(DataWithOutDate);
+    });
+
+    test("test POST /launches", async () => {
+      const response = await request(app)
+        .post("/v1/launches")
+        .send(dataWithInvalidDate)
+        .expect(400);
 
       expect(response.body).toStrictEqual({
-        error:"invalid launch Date"
-      })
-  })
+        error: "invalid launch Date",
+      });
+    });
 
-  test('test POST /launches', async() => {
-    const response = await request(app).post('/launches')
-      .send(DataWithOutDate)
-      .expect(400)
+    test("test POST /launches", async () => {
+      const response = await request(app)
+        .post("/v1/launches")
+        .send(DataWithOutDate)
+        .expect(400);
 
       expect(response.body).toMatchObject({
-        error:"Launch properties are not filled"
-      })
-  })
-});
-
-describe("test DELETE /launches", () => {
-  test("status code should be 200", async () => {
-    const response = await request(app).delete("/launches/101").expect(200);
-    expect(response.body).toMatchObject({
-      upcoming: false,
-      success: false,
+        error: "Launch properties are not filled",
+      });
     });
   });
 });
+
+// describe("test DELETE /launches", () => {
+//   test("status code should be 200", async () => {
+//     const response = await request(app).delete("/launches/101").expect(200);
+//     expect(response.body).toMatchObject({
+//       upcoming: false,
+//       success: false,
+//     });
+//   });
+// });
