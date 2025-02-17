c045dfbf31a62dbe028d2a12b8f481d04cd8c7e5
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-02-18 04:19:59 +0500

    query parameters added in api

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,8 +1,11 @@
 const http = require("http");
 
+require('dotenv').config()
+
 const app = require("./app");
 const { loadPlanetsData } = require("./Models/planets.model");
 const { mongoConnect } = require("./services/mongo");
+const { loadLaunchData } = require("./Models/launches.model");
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,6 +14,7 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
   server.listen(PORT, () => {
     console.log(`listening at port ${PORT}`);
   });
